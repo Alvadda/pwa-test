026ec85bca0179f9f10afa69d0eb498495e2078a
Alvadda <58438582+Alvadda@users.noreply.github.com>
2021-12-21 20:35:27 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack')
+require('dotenv').config({ path: './.env' })
+
+const outputPath = path.resolve(__dirname, 'dist')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: outputPath,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
+  ],
+}
